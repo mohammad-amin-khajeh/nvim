5f11e1c11bfe155ce572ad53949b2a28eb2294a3
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-07-26 12:45:43 +0330

    Switch biome with prettierd for formatting and fix indentation

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,24 +1,24 @@
 local options = {
-	lsp_fallback = true,
+  lsp_fallback = true,
 
-	formatters_by_ft = {
-		c = { "clang-format" },
-		-- css = { { "prettierd", "prettier" } },
-		-- html = { { "prettierd", "prettier" } },
-		javascript = { "biome" },
-		lua = { "stylua" },
-		python = { "ruff" },
-		sh = { "shellharden", "shfmt" },
-		zsh = { "shellharden", "shfmt" },
-		typescript = { "biome" },
-		typescriptreact = { "biome" },
-	},
+  formatters_by_ft = {
+    c = { "clang-format" },
+    css = { "prettierd" },
+    html = { "prettierd" },
+    javascript = { "prettierd" },
+    lua = { "stylua" },
+    python = { "ruff" },
+    sh = { "shellharden", "shfmt" },
+    zsh = { "shellharden", "shfmt" },
+    typescript = { "prettierd" },
+    typescriptreact = { "prettierd" },
+  },
 
-	--[[ format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-		lsp_fallback = true,
-	}, ]]
+  --[[ format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  }, ]]
 }
 
 require("conform").setup(options)
