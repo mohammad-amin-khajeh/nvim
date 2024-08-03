fffdd6ffb8c151538672c61ffc5c71d782d98981
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-08-03 07:45:08 +0330

    Add a neorg workspace and switch to spaces

--- lua/configs/neorg.lua
+++ lua/configs/neorg.lua
@@ -1,18 +1,19 @@
-require("neorg").setup({
-	load = {
-		["core.defaults"] = {}, -- Loads default behaviour
-		["core.export"] = {}, -- Loads default behaviour
-		["core.concealer"] = {}, -- Adds pretty icons to your documents
-		["core.dirman"] = { -- Manages Neorg workspaces
-			config = {
-				workspaces = {
-					english = "~/Documents/notes/english",
-				},
-				default_workspace = "english",
-			},
-		},
-	},
-})
+require("neorg").setup {
+  load = {
+    ["core.defaults"] = {}, -- Loads default behaviour
+    ["core.export"] = {}, -- Loads default behaviour
+    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+    ["core.dirman"] = { -- Manages Neorg workspaces
+      config = {
+        workspaces = {
+          english = "~/Documents/notes/english",
+          code = "~/Documents/code",
+        },
+        default_workspace = "code",
+      },
+    },
+  },
+}
 vim.wo.conceallevel = 2
 -- vim.wo.concealcursor = 1
 vim.wo.foldlevel = 99
