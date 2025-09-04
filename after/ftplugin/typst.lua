2c2a9a3bd6dc8392f90a11574a40f48d6374332a
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-09-04 11:13:12 +0330

    Add $$ to the list of pairs for typst files

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -4,5 +4,11 @@ vim.b.minisurround_config = {
       input = { "%$().-()%$" },
       output = { left = "$", right = "$" },
     },
+    M = {
+      input = { "%$().-()%$" },
+      output = { left = "$ ", right = " $" },
+    },
   },
 }
+
+require("mini.pairs").map_buf(0, "i", "$", { action = "closeopen", pair = "$$" })
