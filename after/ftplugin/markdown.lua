aa229384d8de9ac7bff225bdad7a0908d461dd12
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-09-09 15:13:00 +0330

    Improve nvim.autopair for typst and markdown files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,1 +1,2 @@
 require("mini.pairs").map_buf(0, "i", "*", { action = "closeopen", pair = "**" })
+require("mini.pairs").map_buf(0, "i", "_", { action = "closeopen", pair = "__" })
--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -12,3 +12,5 @@ vim.b.minisurround_config = {
 }
 
 require("mini.pairs").map_buf(0, "i", "$", { action = "closeopen", pair = "$$" })
+require("mini.pairs").map_buf(0, "i", "_", { action = "closeopen", pair = "__" })
+require("mini.pairs").map_buf(0, "i", "*", { action = "closeopen", pair = "**" })
